997bf8498c16c8caea538680bc705c33d88f2d70
Sam Whited <sam@samwhited.com>
2019-06-09 11:09:58 -0500

    mux: expand docs and add examples

---
+++ example_test.go
@@ -0,0 +1,67 @@
+package mux_test
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+
+	"code.soquee.net/mux"
+)
+
+func Example_path() {
+	m := mux.New(
+		mux.HandleFunc("GET", "/sha256/{wildcard path}", func(w http.ResponseWriter, r *http.Request) {
+			val, _ := mux.Param(r, "wildcard")
+			sum := sha256.Sum256([]byte(val.Raw))
+			fmt.Fprintf(w, "the hash of %q is %x", val.Raw, sum)
+		}),
+	)
+
+	server := httptest.NewServer(m)
+	resp, err := http.Get(server.URL + "/sha256/a/b")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	io.Copy(os.Stdout, resp.Body)
+	// Output:
+	// the hash of "a/b" is c14cddc033f64b9dea80ea675cf280a015e672516090a5626781153dc68fea11
+}
+
+func Example_normalization() {
+	m := mux.New(
+		mux.HandleFunc("GET", "/profile/{username string}/personal", func(w http.ResponseWriter, r *http.Request) {
+			username, _ := mux.Param(r, "username")
+			// You probably want to use the Username Case Mapped profile from the
+			// golang.org/x/text/secure/precis package instead of lowercasing.
+			normalized := strings.ToLower(username.Raw)
+
+			// If the username had any capital letters, redirect to the canonical
+			// username.
+			if normalized != username.Raw {
+				newPath := r.URL.Path[:username.Offset] + normalized + r.URL.Path[username.Offset+uint(len(username.Raw)):]
+				http.Redirect(w, r, newPath, http.StatusPermanentRedirect)
+				return
+			}
+
+			// Show the users profile.
+			fmt.Fprintf(w, "Profile for the user %q", username.Raw)
+		}),
+	)
+
+	server := httptest.NewServer(m)
+	resp, err := http.Get(server.URL + "/profile/Me/personal")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	io.Copy(os.Stdout, resp.Body)
+	// Output:
+	// Profile for the user "me"
+}
--- mux.go
+++ mux.go
@@ -1,9 +1,21 @@
-// Package mux is a fast and safe HTTP multiplexer.
+// Package mux is a fast and safe HTTP request multiplexer.
+//
+// The muxer in this package is capable of routing based on request method and a
+// fixed rooted path (/favicon.ico) or subtree (/images/) which may include
+// typed path parameters and wildcards (see "URL Parameters").
+//
+//	m := mux.New(
+//		mux.HandleFunc(http.MethodGet, "/profile/{username string}", http.NotFoundHandler())
+//		mux.HandleFunc(http.MethodGet, "/profile", http.RedirectHandler("/profile/me", http.StatusPermanentRedirect))
+//		mux.HandleFunc(http.MethodPost, "/logout", logoutHandler())
+//	)
 //
 // URL Parameters
 //
 // Routes registered on the multiplexer may contain variable path parameters
 // that comprise an optional name, followed by a type.
+//
+//     /user/{id int}/edit
 //
 // Valid types include:
 //
@@ -14,15 +26,13 @@
 //     path   eg. files/123.png (must be the last path component)
 //
 // All numeric types are 64 bits wide.
-// A non-path typed variable parameter may appear anywhere in the path and match
-// a single path component:
-//
-//     /user/{id int}/edit
-//
 // Parameters of type "path" match the remainder of the input path and therefore
 // may only appear as the final component of a route:
 //
 //     /file/{p path}
+//
+// To retrieve the value of named path parameters see the Param function and the
+// examples.
 //
 // Two paths with different typed variable parameters (including static routes)
 // in the same position are not allowed.
@@ -30,6 +40,7 @@
 //
 //     /user/{a int}/new
 //     /user/{b int}/edit
+//     /user/{float}/edit
 //     /user/{b string}/edit
 //     /user/me
 package mux // import "code.soquee.net/mux"
